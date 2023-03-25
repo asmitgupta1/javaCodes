c89b73ccb8e25a2c94b10fdf2a6428ecbc442c2b
asmitgupta1 <guptaasmit1@gmail.com>
2023-03-25 16:09:36 +0530

    adfhgui

---
+++ src/AnonymousInnerClass/shop/Door.java
@@ -0,0 +1,22 @@
+package AnonymousInnerClass.shop;
+
+
+
+public class Door{
+	private Lock lock = new Lock(){
+
+		@Override
+		public boolean isUnlocked(String keyCode) {
+			if(keyCode.equals("qwerty")){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		
+	};
+	public Lock getLock() {
+		return lock;
+	}
+	
+}
--- src/InnerClasses/Shop.java
+++ src/InnerClasses/Shop.java
@@ -1,9 +1,20 @@
 package InnerClasses;
 
+import InnerClasses.shop.Door;
+
 public class Shop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Door door = new Door();
+		
+		door.shopStatus();
+		
+		door.getLock().setLock(false);
+		
+		door.shopStatus();
+		
+		
 
 	}
 
